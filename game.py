07c6cad110c81edd5a2bc9d39cb0ad77c589f353
412-39-prapatsara <56784@yupparaj.ac.th>
2026-09-15 10:14:46 +0700

    Update game.py

--- game.py
+++ game.py
@@ -107,7 +107,7 @@ def play_game():
 
 if __name__ == "__main__":
        play_game()
-commit -m "Add Thai Food Guessing Game"
+commit "Add Thai Food Guessing Game"
 git branch -M main
 git remote add origin https://github.com/USERNAME/REPOSITORY_NAME.git
 git push -u origin main
